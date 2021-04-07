fbc5250707c11c5795ec73b3159677f9d790693f
JustAnother-Programmer <60672202+JustAnother-Programmer@users.noreply.github.com>
2021-04-07 18:54:19 +0100

    Added .env support

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,6 @@
+import { config } from 'dotenv'
+config({ path: '.env' })
+
 import { Client } from 'discord.js'
 
 const bot = new Client()
@@ -12,4 +15,4 @@ bot.on('message', async (msg) => {
     await msg.reply('Hello!')
 })
 
-bot.login('')
+bot.login(process.env.BOT_TOKEN)
