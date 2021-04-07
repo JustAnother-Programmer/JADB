148ad007e4ae547ff3e198726a1c90f9d1b77b8b
JustAnother-Programmer <60672202+JustAnother-Programmer@users.noreply.github.com>
2021-04-07 19:56:18 +0100

    Update ping command to include an actual number

--- src/handlers/commands/ping.js
+++ src/handlers/commands/ping.js
@@ -1,9 +1,11 @@
+import { Client } from "discord.js"
 import Command from "./command.js"
+import Deps from '../../utils/deps.js'
 
 export default class extends Command {
     name = 'ping'
 
     async execute(msg) {
-        await msg.reply('Pong!')
+        await msg.reply(`🏓 Pong! ${Math.round(Deps.get(Client).ws.ping)}ms`)
     }
 }
