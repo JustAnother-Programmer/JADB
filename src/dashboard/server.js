1b72df5d0e1e8bd9da86739cd337b9e78cc05157
JustAnother-Programmer <60672202+JustAnother-Programmer@users.noreply.github.com>
2021-04-10 12:54:01 +0100

    Add command page, update overall look, add buttons and styles, add favicons

--- src/dashboard/server.js
+++ src/dashboard/server.js
@@ -6,12 +6,29 @@ const app = express()
 const port = process.env.PORT || 3000
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-app.set('views', __dirname + '/views')
+app.use(express.static(__dirname + '/public'))
+app.set('views', __dirname + '/public/views')
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
     res.render('index', {
         subtitle: 'Home'
+    })
+})
+
+app.get('/commands', (req, res) => {
+    res.render('commands', {
+        subtitle: 'Commands',
+        categories: [
+            { name: 'General', icon: 'fas fa-star' },
+            { name: 'Economy', icon: 'fas fa-coins' }, 
+            { name: 'Administration', icon: 'fas fa-gavel' }, 
+            { name: 'Music', icon: 'fas fa-music' }, 
+        ],
+        commands: [
+            { name: 'ping', category: 'General' },
+            { name: 'prefix', category: 'General' }
+        ]
     })
 })
 
